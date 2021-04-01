e4e8c8ca7fca2370f885876d449f10867c6976c9
daniels9 <d.schweimer@web.de>
2021-04-01 14:22:32 +0200

    Initial Commit of Data

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-import pandas as pd 
+import pandas as pd
+
+test = pd.read_csv("C:/Users/dschw/Documents/GitHub/Thesis/Data/NCCR_Content/NCCR_Content/txt_table/Text_Table.txt",
+            delimiter = "\t", encoding = "ISO-8859-1")
+
+
+# def main (input_path,
+#           import_NCCR):
+#
+#     if import_NCCR:
+#         table_text = pd.read_csv(f'{input_path}\\Data\\NCCR_Content\\NCCR_Content\\Text_Table.txt',
+#                                  delimiter="\t", encoding="ISO-8859-1")
+#
+# if __name__ == "__main__":
+#     parser = ArgumentParser()
+#     parser.add_argument("-i", "--input", type=str,
+#                         help="path to project", metavar="path")
+#     args = parser.parse_args()
+#     input_path = args.input
+#
+#     main(input_path=input_path,
+#          import_NCCR=True)
