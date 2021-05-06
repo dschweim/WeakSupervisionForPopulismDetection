8a78a7a847e07c79e29dd97e4fcc11fefe1109dc
daniels9 <33021140+daniels9@users.noreply.github.com>
2021-05-06 23:13:45 +0200

    Include train_dev_test split fct

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 
 def generate_train_test_split(df):
@@ -14,3 +15,18 @@ def generate_train_test_split(df):
 
     return train, test
 
+
+def generate_train_dev_test_split(df):
+    """
+    Generate train dev test split
+    :param df: Dataset to split
+    :type df: DataFrame
+    :return: Returns three datasets
+    :rtype: DataFrame, DataFrame, DataFrame
+    """
+
+    # Split into 60% train, 20% dev and 20% test
+    train, dev, test = np.split(df.sample(frac=1, random_state=42),
+                                [int(.6 * len(df)), int(.8 * len(df))])
+
+    return train, dev, test
